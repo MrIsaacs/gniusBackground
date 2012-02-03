03e6599fa9bd7b3730bd63589578fda07ddf929e
Ivan Ilić <ivan.isaacs@windowslive.com>
2012-02-03 15:39:49 +0100

    mootools/forge update test

--- Source/gniusBackground.js
+++ Source/gniusBackground.js
@@ -1,83 +1,117 @@
 /*
 ---
-description: Resizes the "background-image" depended on its aspec ratio and with centered to a focus point.
 
-license: MIT-style
+name: Form.MultipleFileInput
+description: Create a list of files that has to be uploaded
+license: MIT-style license.
+authors: Arian Stolwijk
+requires: [Element.Event, Class, Options, Events]
+provides: Form.MultipleFileInput
 
-authors:
-- Ivan Ilić
-
-requires:
-  - core/1.4.1: Dimensions
-
-provides: 
-  - gniusBackground
 ...
 */
 
-var gniusBackground = new Class({
-    Implements      : Options,
 
-    options         : {
-        scrollable  : false,
-        width       : 1440,
-        height      : 900
-    },
+Object.append(Element.NativeEvents, {
+	dragenter: 2, dragleave: 2, dragover: 2, dragend: 2, drop: 2
+});
 
-    initialize      : function (element, options) {
-        var that = this;
-        this.setOptions(options);
+if (!this.Form) this.Form = {};
 
-        window.addEvents({
-            domready    : function () {
-                that.gniusBackground($(element));
-            },
-            resize      : function () {
-                that.gniusBackground($(element));
-            }
-        });
-    },
+Form.MultipleFileInput = new Class({
 
-    gniusBackground : function (element) {
-        var imageRatio  = this.options.width / this.options.height,
-            windowRatio = window.getSize().x / window.getSize().y,
-            gniusImg    = element.getChildren()[0];
+	Implements: [Options, Events],
 
-        //width of window is more than width of image
-        if (windowRatio > imageRatio) {
-            gniusImg.setStyles({
-                width   : window.getSize().x + 2,
-                height  : window.getSize().x / imageRatio
-            });
-        } else {
-            gniusImg.setStyles({
-                width   : window.getSize().y / (1 / imageRatio),
-                height  : window.getSize().y + 1
-            });
-        }
+	options: {
+		itemClass: 'uploadItem'/*,
+		onAdd: function(file){},
+		onRemove: function(file){},
+		onEmpty: function(){},
+		onDragenter: function(event){},
+		onDragleave: function(event){},
+		onDragover: function(event){},
+		onDrop: function(event){}*/
+	},
 
-        if (!this.options.scrollable) {
-            // Set not scrollable Style behaviour.
-            element.setStyles({
-                'position'      : 'fixed',
-                'overflow'      : 'hidden',
-                'margin-top'    : -(gniusImg.getSize().y / 2) - 1,
-                'margin-left'   : -(gniusImg.getSize().x / 2) - 1
-            });
-        } else {
-            // Set scrollable Style behaviour.
-            $('body').setStyles({
-                'overflow-x'    : 'hidden'
-            });
+	_files: [],
 
-            element.setStyles({
-                'position'      : 'absolute',
-                'overflow-x'    : 'hidden',
-                'margin-top'    : (-gniusImg.getSize().y / 2) - 1,
-                'margin-left'   : -(gniusImg.getStyle('width').toInt() / 2),
-                'height'        : gniusImg.getSize().y,
-                'width'         : ((window.getSize().x + gniusImg.getSize().x) / 2) + 1
-            });
-        }
-    }
+	initialize: function(input, list, drop, options){
+		input = this.element = document.id(input);
+		list = this.list = document.id(list);
+		drop = this.drop = document.id(drop);
+
+		this.setOptions(options);
+
+		var name = input.get('name');
+		if (name.slice(-2) != '[]') input.set('name', name + '[]');
+		input.set('multiple', true);
+
+		this.inputEvents = {
+			change: function(){
+				Array.each(input.files, this.add, this);
+			}.bind(this)
+		};
+
+		this.dragEvents = drop && (typeof document.body.draggable != 'undefined') ? {
+			dragenter: this.fireEvent.bind(this, 'dragenter'),
+			dragleave: this.fireEvent.bind(this, 'dragleave'),
+			dragend: this.fireEvent.bind(this, 'dragend'),
+			dragover: function(event){
+				event.preventDefault();
+				this.fireEvent('dragover', event);
+			}.bind(this),
+			drop: function(event){
+				event.preventDefault();
+				var dataTransfer = event.event.dataTransfer;
+				if (dataTransfer) Array.each(dataTransfer.files, this.add, this);
+				this.fireEvent('drop', event);
+			}.bind(this)
+		} : null;
+
+		this.attach();
+	},
+
+	attach: function(){
+		this.element.addEvents(this.inputEvents);
+		if (this.dragEvents) this.drop.addEvents(this.dragEvents);
+	},
+
+	detach: function(){
+		this.input.removeEvents(this.inputEvents);
+		if (this.dragEvents) this.drop.removeEvents(this.dragEvents);
+	},
+
+	add: function(file){
+		this._files.push(file);
+		var self = this;
+		new Element('li', {
+			'class': this.options.itemClass
+		}).grab(new Element('span', {
+			text: file.name
+		})).grab(new Element('a', {
+			text: 'x',
+			href: '#',
+			events: {click: function(e){
+				e.preventDefault();
+				self.remove(file);
+			}}
+		})).inject(this.list);
+		this.fireEvent('add', file);
+		return this;
+	},
+
+	remove: function(file){
+		var index = this._files.indexOf(file);
+		if (index == -1) return this;
+		this._files.splice(index, 1);
+		this.list.childNodes[index].destroy();
+		this.fireEvent('remove', file);
+		if (!this._files.length) this.fireEvent('empty');
+		return this;
+	},
+
+	getFiles: function(){
+		return this._files;
+	}
+
 });
